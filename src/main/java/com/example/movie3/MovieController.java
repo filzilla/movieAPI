e0981f02bdbc6a3d2482defe4578a36fabe84e8d
filzilla <phillipdelia@Phillips-MacBook-Pro.local>
2017-09-18 17:17:38 -0400

    complete

--- src/main/java/com/example/movie3/MovieController.java
+++ src/main/java/com/example/movie3/MovieController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by phillipdelia on 9/18/17.
@@ -33,6 +34,14 @@ public class MovieController {
 
     @RequestMapping("/medium-popular-long-name")
     public String mediumPopular(Model model){
+
+             List<Movie> movies = getMovies(url)
+                .stream()
+                .filter(movie -> movie.getPopularity() >= 30 && movie.getPopularity() <= 80)
+                .filter(movie -> movie.getTitle().length() >= 10)
+                .collect(Collectors.toList());
+             model.addAttribute( "movies", movies);
+
 
             return "medium-popular-long-name";
     }
